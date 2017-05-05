0d9b3f0fb4ad4ea053765f1ebbac1d4f6cc9b49c
薛依玲 <wb-xyl259837@alibaba-inc.com>
2017-05-05 18:09:43 +0800

    add proj

---
+++ proj/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>This is a empty page.</div>
+  }
+}
+
+export default App;
